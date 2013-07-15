2e2e121479f5d7d271fca72e80cec2ea9bba44bf
Angel Calayag <angel.calayag@gmail.com>
2013-07-15 10:52:47 +0000

    debugged

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var data;
 
 app.get('/', function(request, response) {
   //response.send('Hello World2!');
-  data = fs.readFileSync('index.html');
-  response.send(data);
+  fs = require("fs");
+  var line = fs.readFileSync('index.html');
+  response.send(line);
 });
 
 var port = process.env.PORT || 5000;
