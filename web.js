d2488692174ec979243d5a18d9615f472d8373dd
Angel Calayag <angel.calayag@gmail.com>
2013-07-15 10:38:31 +0000

    modified to read from file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer(16);
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  buffer = fs.readFile('/index.html');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
