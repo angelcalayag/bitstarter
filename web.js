218993e29f619dd9b9e7acf2de6bfd343f9977c6
Angel Calayag <angel.calayag@gmail.com>
2013-07-15 10:59:01 +0000

    used buffer

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer(16);
 
 app.get('/', function(request, response) {
   //response.send('Hello World2!');
   fs = require("fs");
-  var line = fs.readFileSync('index.html');
-  response.send(line);
+  buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString('buffer'));
 });
 
 var port = process.env.PORT || 5000;
